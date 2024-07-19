7c3b8576ae9db2edfac76b6ca8a11ae01218fbb3
abdul <11743617+bralat@users.noreply.github.com>
2024-07-19 09:55:58 +0100

    database only allows connections from the api

--- vpc.tf
+++ vpc.tf
@@ -99,13 +99,15 @@ resource "aws_security_group" "database" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "only_allow_api" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.database.id # target security group
+  source_security_group_id = aws_security_group.api.id
 }
 
 resource "aws_security_group" "api" {
