d19836743fb7fb78e087c87c2b8777180f982924
abdul <11743617+bralat@users.noreply.github.com>
2024-07-19 09:51:52 +0100

    ecs only allows connections from the load balancer

--- vpc.tf
+++ vpc.tf
@@ -119,11 +119,13 @@ resource "aws_security_group" "api" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "only_allow_lb" {
+  type                     = "ingress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.api.id # target security group
+  source_security_group_id = aws_security_group.alb.id
 }
