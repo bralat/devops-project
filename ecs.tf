932acb5123469d2aa741071db247cd1af6d8bd37
abdul <11743617+bralat@users.noreply.github.com>
2024-07-19 20:59:29 +0100

    ecs autoscaling logic added

--- ecs.tf
+++ ecs.tf
@@ -64,3 +64,85 @@ resource "aws_ecs_task_definition" "helloworld" {
     ]
   )
 }
+
+resource "aws_appautoscaling_target" "api_autoscaling" {
+  max_capacity       = 3
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.api.name}/${aws_ecs_service.helloworld.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "scale_up" {
+  name               = "scale-up"
+  policy_type        = "StepScaling"
+  resource_id        = aws_appautoscaling_target.api_autoscaling.resource_id
+  scalable_dimension = aws_appautoscaling_target.api_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.api_autoscaling.service_namespace
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      scaling_adjustment          = 1
+      metric_interval_lower_bound = 0
+    }
+  }
+}
+
+resource "aws_appautoscaling_policy" "scale_down" {
+  name               = "scale-down"
+  policy_type        = "StepScaling"
+  resource_id        = aws_appautoscaling_target.api_autoscaling.resource_id
+  scalable_dimension = aws_appautoscaling_target.api_autoscaling.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.api_autoscaling.service_namespace
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      scaling_adjustment          = -1
+      metric_interval_lower_bound = 0
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_usage" {
+  alarm_name          = "high-cpu-usage"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_description   = "Trigger an alarm when the CPU usage exceeds 80% for 2 consecutive periods"
+  dimensions = {
+    ClusterName = aws_ecs_cluster.api.name
+    ServiceName = aws_ecs_service.helloworld.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.scale_up.arn]
+}
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu_usage" {
+  alarm_name          = "low-cpu-usage"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "20"
+  alarm_description   = "Trigger task scale down when the CPU usage dips below 20% for 2 consecutive periods"
+  dimensions = {
+    ClusterName = aws_ecs_cluster.api.name
+    ServiceName = aws_ecs_service.helloworld.name
+  }
+
+  alarm_actions = [aws_appautoscaling_policy.scale_up.arn]
+}
