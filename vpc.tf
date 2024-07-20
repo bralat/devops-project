0df2142bcb4534d917be564da7d93c6845fac455
abdul <11743617+bralat@users.noreply.github.com>
2024-07-20 02:58:25 +0100

    security group for ecs allows port 8000

--- vpc.tf
+++ vpc.tf
@@ -132,8 +132,8 @@ resource "aws_security_group" "api" {
 
 resource "aws_security_group_rule" "only_allow_lb" {
   type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
+  from_port                = 8000
+  to_port                  = 8000
   protocol                 = "tcp"
   security_group_id        = aws_security_group.api.id # target security group
   source_security_group_id = aws_security_group.alb.id
