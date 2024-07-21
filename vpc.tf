d341655c485d9778e6a18c6076a0e6827cfbf570
abdul <11743617+bralat@users.noreply.github.com>
2024-07-22 00:52:45 +0100

    subnets sliced into smaller pieces for convenience

--- vpc.tf
+++ vpc.tf
@@ -6,28 +6,28 @@ resource "aws_vpc" "vpc" {
 
 resource "aws_subnet" "public-eu-west-2a" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.0.0/20"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "public-eu-west-2b" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.128.0/20"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "eu-west-2b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private-eu-west-2a" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.16.0/20"
+  cidr_block              = "10.0.3.0/24"
   availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = false
 }
 
 resource "aws_subnet" "private-eu-west-2b" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.144.0/20"
+  cidr_block              = "10.0.4.0/24"
   availability_zone       = "eu-west-2b"
   map_public_ip_on_launch = false
 }
